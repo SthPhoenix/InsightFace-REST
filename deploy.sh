b9e959c507e08e8238d873c502bf1d09004742c7
SthPhoenix <veloce-raptor@bk.ru>
2020-02-18 20:59:01 +0300

    Update version and add some variables

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
 IMAGE='insightface-rest'
-TAG='v0.1'
+TAG='v0.1.2'
 
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=$IMAGE:$TAG --format="{{.ID}}"))
 docker image rm $IMAGE:$TAG
 
 docker build -t $IMAGE:$TAG .
 
-docker run  -p 6000:6000 $IMAGE:$TAG
+docker run  -p 18080:18080\
+    -e PYTHONUNBUFFERED=0\
+    -e PORT=18080\
+    -e MAX_SIZE=640\
+     $IMAGE:$TAG
 
