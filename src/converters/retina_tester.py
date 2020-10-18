414c7bdc01a2a9d9fd7a99d70af65e364049d436
SthPhoenix <veloce-raptor@bk.ru>
2020-10-19 00:22:14 +0300

    Fix duplicate boxes on output image

--- src/converters/retina_tester.py
+++ src/converters/retina_tester.py
@@ -20,8 +20,8 @@ config = Configs(models_dir='/models')
 
 iters = 100
 
-model_name = 'retinaface_mnet025_v2'
-#model_name = 'retinaface_r50_v1'
+#model_name = 'retinaface_mnet025_v2'
+model_name = 'retinaface_r50_v1'
 input_shape = [1024, 768]
 
 # 'plan' for TensorRT or 'onnx' for ONNX
@@ -41,7 +41,7 @@ detector = FaceDetector(inference_backend=retina_backend)
 detector.prepare(fix_image_size=(input_shape[1], input_shape[0]))
 
 tt0 = time.time()
-image = cv2.imread('test_images/Stallone.jpg', cv2.IMREAD_COLOR)
+image = cv2.imread('test_images/Stallone.jpg.jpg', cv2.IMREAD_COLOR)
 image = ImageData(image, input_shape)
 image.resize_image(mode='pad')
 tt1 = time.time()
@@ -54,18 +54,19 @@ for i in range(iters):
     detections = detector.detect(image.transformed_image, threshold=0.3)
     tf1 = time.time()
     print(f"Full detection  took: {tf1 - tf0}")
-    for res in detections[0]:
-        if res[4] > 0.5:
-            color = (0, 255, 0)
-        else:
-            color = (0, 0, 255)
-        #print(res[4])
-        pt1 = tuple(map(int, res[0:2]))
-        pt2 = tuple(map(int, res[2:4]))
-        cv2.rectangle(image.transformed_image, pt1, pt2, color, 1)
 
 t1 = time.time()
 print(f'Took {t1 - t0} s. ({iters / (t1 - t0)} im/sec)')
+
+for det in detections[0]:
+    if det[4] > 0.5:
+        color = (0, 255, 0)
+    else:
+        color = (0, 0, 255)
+    # print(res[4])
+    pt1 = tuple(map(int, det[0:2]))
+    pt2 = tuple(map(int, det[2:4]))
+    cv2.rectangle(image.transformed_image, pt1, pt2, color, 1)
 
 cv2.imwrite('test_retina.jpg', image.transformed_image)
 
