dc7be78294f443c8e6c54408a526cfbdf7622b76
SthPhoenix <veloce-raptor@bk.ru>
2020-10-19 02:25:56 +0300

    Fix NMS import

--- src/converters/detectors/retina.py
+++ src/converters/detectors/retina.py
@@ -353,42 +353,10 @@ class FaceDetector:
             landmarks = landmarks[order].astype(np.float32, copy=False)
 
         pre_det = np.hstack((proposals[:,0:4], scores)).astype(np.float32, copy=False)
-        #keep = self.nms(pre_det)
-        keep = nms2(pre_det,thresh = self.nms_threshold)
+        keep = nms(pre_det,thresh = self.nms_threshold)
         det = np.hstack( (pre_det, proposals[:,4:]) )
         det = det[keep, :]
         if self.use_landmarks:
             landmarks = landmarks[keep]
 
         return det, landmarks
-
-    def nms(self, dets, thresh = 0.4):
-        thresh = self.nms_threshold
-        x1 = dets[:, 0]
-        y1 = dets[:, 1]
-        x2 = dets[:, 2]
-        y2 = dets[:, 3]
-        scores = dets[:, 4]
-
-        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-        order = scores.argsort()[::-1]
-
-        keep = []
-        while order.size > 0:
-            i = order[0]
-            keep.append(i)
-            xx1 = np.maximum(x1[i], x1[order[1:]])
-            yy1 = np.maximum(y1[i], y1[order[1:]])
-            xx2 = np.minimum(x2[i], x2[order[1:]])
-            yy2 = np.minimum(y2[i], y2[order[1:]])
-
-            w = np.maximum(0.0, xx2 - xx1 + 1)
-            h = np.maximum(0.0, yy2 - yy1 + 1)
-            inter = w * h
-            ovr = inter / (areas[i] + areas[order[1:]] - inter)
-
-            inds = np.where(ovr <= thresh)[0]
-            order = order[inds + 1]
-
-        return keep
-
