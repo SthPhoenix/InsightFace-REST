e28a13229559d6c2ec846c1162a13a0cd249282e
SthPhoenix <veloce-raptor@bk.ru>
2020-10-23 23:01:44 +0300

    Added documentation and argument to change number of uvicorn workers

--- deploy_trt.sh
+++ deploy_trt.sh
@@ -1,67 +1,100 @@
 #! /bin/bash
 
 IMAGE='insightface-rest'
-TAG='v0.5.5'
+TAG='v0.5.6'
 
-#When starting multiple containers this will be port assigned to first container
+# When starting multiple containers this will be port assigned to first container
 START_PORT=18081
 
-#Set number of GPU's availiable in your system
+# Set number of GPU's availiable in your system
 n_gpu=1
-#Set how many containers you want to run per GPU
-n_con=1
+
+# Set how many app instances you want to run per GPU, ensure you have enough GPU
+# memory for desired number. Try running with n_workers=1 to estimate memory consumption
+# per instance.
+# Take note: larger number won't speed up single image inference time, it'll increase
+# concurrent throughput.
+n_workers=1
+
+# Maximum image size (W,H). If your input images has fixed image size set this
+# value proportional or equal to it. Otherwise select value based on your
+# performance/accuracy needs.
+# If input images may have both album/portrait orientation it's recommended to
+# set square dimensions, like 640x640 for better accuracy.
+# ATTENTION: For TensorRT backend this size currently can't be set during
+# runtime.
+max_size=640,640
+
+# DET MODELS:
+## retinaface_mnet025_v1, retinaface_mnet025_v2, retinaface_r50_v1, centerface
+## ATTENTION: mtcnn model is not supported yet for TensorRT backend.
+det_model=retinaface_mnet025_v2
+
+# REC MODELS:
+## arcface_r100_v1
+rec_model=arcface_r100_v1
+## Do not load recognition model:
+rec_ignore=False
+
+# GENDER/AGE MODELS:
+## genderage_v1
+ga_model=genderage_v1
+## Do not load genderage model:
+ga_ignore=False
+
+# Default settings for inference requests, can be overridden inside
+# request body.
+
+## Return base64 encoded face crops.
+return_face_data=False
+## Get faces embeddings. Otherwise only bounding boxes will be returned.
+extract_embeddings=True
+## Estimate gender/age
+detect_ga=True
 
 
-# Calculate number of CPU cores required to run desired number of containers.
-# By default every container will be assigned to 1 core.
-ncpu=$((n_gpu * n_con))
-cpus=$(( ncpu / (n_gpu * n_con) ))
+# DEPLOY CONTAINERS
 
-
-#Create directory to store downloaded models
+# Create directory to store downloaded models
 mkdir -p models
 
-echo "Starting $((n_gpu * n_con)) containers on $n_gpu GPUs ($n_con containers per GPU)";
-echo "Containers port range: $START_PORT - $(($START_PORT + ($n_gpu* $n_con) - 1))"
+echo "Starting $((n_gpu)) containers on $n_gpu GPUs ($n_workers workers per GPU)";
+echo "Containers port range: $START_PORT - $(($START_PORT + ($n_gpu) - 1))"
 
 docker build -t $IMAGE:$TAG -f src/Dockerfile_trt src/.
 
 p=0
+
 for i in $(seq 0 $(($n_gpu - 1)) ); do
     device='"device='$i'"';
-    for j in $(seq 0 $(($n_con - 1))); do
+    port=$((START_PORT + $p));
+    name=$IMAGE-gpu$i-trt;
 
-        port=$((START_PORT + $p));
-        name=$IMAGE-g$i-$j-trt;
-
-        docker stop $name;
-        docker rm $name;
-        echo --- Starting container $name  with $device  at port $port;
-        ((p++));
-        docker run  -p $port:18080\
-            --gpus $device\
-            -d\
-            -e PYTHONUNBUFFERED=0\
-            -e PORT=18080\
-            -e INFERENCE_BACKEND=trt\
-            -e DET_NAME=retinaface_mnet025_v2\
-            -e REC_NAME=arcface_r100_v1\
-            -e GA_NAME=genderage_v1\
-            -e GA_IGNORE=False\
-            -e KEEP_ALL=True\
-            -e MIN_FACE_SIZE=20\
-            -e MTCNN_FACTOR=0.700\
-            -e REC_IGNORE=False\
-            -e MAX_SIZE=640,640\
-            -e DEF_RETURN_FACE_DATA=False\
-            -e DEF_EXTRACT_EMBEDDING=True\
-            -e DEF_EXTRACT_GA=False\
-            -e DEF_API_VER='1'\
-            -v $PWD/models:/models\
-            --name=$name\
-            $IMAGE:$TAG
-    done
+    docker stop $name;
+    docker rm $name;
+    echo --- Starting container $name  with $device  at port $port;
+    ((p++));
+    docker run  -p $port:18080\
+        --gpus $device\
+        -d\
+        -e PYTHONUNBUFFERED=0\
+        -e PORT=18080\
+        -e NUM_WORKERS=$n_workers\
+        -e INFERENCE_BACKEND=trt\
+        -e DET_NAME=$det_model\
+        -e REC_NAME=$rec_model\
+        -e REC_IGNORE=$rec_ignore\
+        -e GA_NAME=$ga_model\
+        -e GA_IGNORE=$ga_ignore\
+        -e KEEP_ALL=True\
+        -e MAX_SIZE=$max_size\
+        -e DEF_RETURN_FACE_DATA=$return_face_data\
+        -e DEF_EXTRACT_EMBEDDING=$extract_embeddings\
+        -e DEF_EXTRACT_GA=$detect_ga\
+        -e DEF_API_VER='1'\
+        -v $PWD/models:/models\
+        -v $PWD/src/api_trt:/app\
+        --name=$name\
+        $IMAGE:$TAG
 done
 
-#DET MODELS:
-#mtcnn, retinaface_mnet025_v1, retinaface_mnet025_v2, retinaface_r50_v1, centerface
