f936d6cd98f8c856c755c469ac69a204d93f5598
SthPhoenix <veloce-raptor@bk.ru>
2020-11-20 12:43:02 +0300

    Add call to prepare_models.py before running workers

--- src/api_trt/entrypoint.sh
+++ src/api_trt/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo Preparing models...
+python prepare_models.py
+
 echo Starting InsightFace-REST using $NUM_WORKERS workers.
 uvicorn fapp:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS
