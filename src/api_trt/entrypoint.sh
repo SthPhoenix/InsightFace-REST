8d827ecabf289864761b75c95e7b8e7b7132b8ec
SthPhoenix <veloce-raptor@bk.ru>
2020-11-20 13:02:48 +0300

    Rename fapp.py to app.py

--- src/api_trt/entrypoint.sh
+++ src/api_trt/entrypoint.sh
@@ -4,4 +4,4 @@ echo Preparing models...
 python prepare_models.py
 
 echo Starting InsightFace-REST using $NUM_WORKERS workers.
-uvicorn fapp:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS
+uvicorn app:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS
