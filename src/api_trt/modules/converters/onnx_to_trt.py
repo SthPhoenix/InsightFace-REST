9418a7b80e54b51ca43832e17cf3f4fb3e71d9e9
SthPhoenix <veloce-raptor@bk.ru>
2020-11-20 12:49:34 +0300

    Add support for converting serialized in-memory ONNX. Add reading input name and shapes form ONNX model for adding optimization profiles.

--- src/api_trt/modules/converters/onnx_to_trt.py
+++ src/api_trt/modules/converters/onnx_to_trt.py
@@ -1,7 +1,7 @@
 import os
 import tensorrt as trt
 import sys
-from typing import Tuple
+from typing import Tuple, Union
 import logging
 
 # Based on code from NVES_R's response at
@@ -12,16 +12,15 @@ TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
 
 
-def _build_engine_onnx(onnx_path: str, force_fp16: bool = False, max_batch_size: int = 1, im_size: Tuple[int] = None):
-    '''
+def _build_engine_onnx(input_onnx: Union[str, bytes], force_fp16: bool = False, max_batch_size: int = 1):
+    """
     Builds TensorRT engine from provided ONNX file
 
-    :param onnx_path: Path to ONNX file on disk
+    :param input_onnx: serialized ONNX model.
     :param force_fp16: Force use of FP16 precision, even if device doesn't support it. Be careful.
     :param max_batch_size: Define maximum batch size supported by engine. If >1 creates optimization profile.
-    :param im_size: Required if max_batch_size > 1. Used for creation of optimization profile.
     :return: TensorRT engine
-    '''
+    """
 
     with trt.Builder(TRT_LOGGER) as builder, \
             builder.create_network(EXPLICIT_BATCH) as network, \
@@ -33,33 +32,35 @@ def _build_engine_onnx(onnx_path: str, force_fp16: bool = False, max_batch_size:
             builder.fp16_mode = True
             builder.strict_type_constraints = True
 
-        builder.max_workspace_size = 1 << 20
+        builder.max_workspace_size = 1 << 30
 
-        if max_batch_size != 1 and im_size is not None:
+        if not parser.parse(input_onnx):
+            print('ERROR: Failed to parse the ONNX')
+            for error in range(parser.num_errors):
+                print(parser.get_error(error))
+            sys.exit(1)
+
+        if max_batch_size != 1:
             logging.warning('Batch size !=1 is used. Ensure your inference code supports it.')
             profile = builder.create_optimization_profile()
-            profile.set_shape('data', (1, 3) + im_size, (max_batch_size, 3) + im_size, (max_batch_size, 3) + im_size)
+            # Get input name and shape for building optimization profile
+            input = network.get_input(0)
+            im_size = input.shape[2:]
+            input_name = input.name
+            profile.set_shape(input_name, (1, 3) + im_size, (1, 3) + im_size, (max_batch_size, 3) + im_size)
             config.add_optimization_profile(profile)
-
-        with open(onnx_path, "rb") as f:
-            if not parser.parse(f.read()):
-                print('ERROR: Failed to parse the ONNX file: {}'.format(onnx_path))
-                for error in range(parser.num_errors):
-                    print(parser.get_error(error))
-                sys.exit(1)
-
-            if max_batch_size != 1:
-                return builder.build_engine(network, config=config)
-            else:
-                return builder.build_cuda_engine(network)
+            return builder.build_engine(network, config=config)
+        else:
+            return builder.build_cuda_engine(network)
 
 
-def convert_onnx(onnx_path: str, engine_file_path: str, force_fp16: bool = False, max_batch_size: int = 1,
+def convert_onnx(input_onnx: Union[str, bytes], engine_file_path: str, force_fp16: bool = False,
+                 max_batch_size: int = 1,
                  im_size: Tuple[int] = None):
     '''
     Creates TensorRT engine and serializes it to disk
 
-    :param onnx_path: Path to ONNX file on disk
+    :param input_onnx: Path to ONNX file on disk or serialized ONNX model.
     :param engine_file_path: Path where TensorRT engine should be saved.
     :param force_fp16: Force use of FP16 precision, even if device doesn't support it. Be careful.
     :param max_batch_size: Define maximum batch size supported by engine. If >1 creates optimization profile.
@@ -67,9 +68,17 @@ def convert_onnx(onnx_path: str, engine_file_path: str, force_fp16: bool = False
     :return: None
     '''
 
-    engine = _build_engine_onnx(onnx_path=onnx_path,
-                                force_fp16=force_fp16, max_batch_size=max_batch_size,
-                                im_size=im_size)
+    onnx_obj = None
+    if isinstance(input_onnx, str):
+        with open(input_onnx, "rb") as f:
+            onnx_obj = f.read()
+    elif isinstance(input_onnx, bytes):
+        onnx_obj = input_onnx
+
+    engine = _build_engine_onnx(input_onnx=onnx_obj,
+                                force_fp16=force_fp16, max_batch_size=max_batch_size)
+
+    assert not isinstance(engine, type(None))
 
     with open(engine_file_path, "wb") as f:
         f.write(engine.serialize())
