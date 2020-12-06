881dd5411e6a544252e5e329d45a9cd214f22299
SthPhoenix <veloce-raptor@bk.ru>
2020-12-06 12:53:36 +0300

    Add draft version of Triton Inference Server Support

--- src/api_trt/modules/model_zoo/exec_backends/triton_backend.py
+++ src/api_trt/modules/model_zoo/exec_backends/triton_backend.py
@@ -8,6 +8,7 @@ import numpy as np
 import tritonclient.http as httpclient
 from tritonclient.utils import triton_to_np_dtype
 from tritonclient.utils import InferenceServerException
+import tritonclient.utils.shared_memory as shm
 
 from numpy.linalg import norm
 
@@ -75,15 +76,15 @@ model_version = '1'
 
 class Arcface:
 
-    def __init__(self, rec_name = 'arcface_r100_v1', model_version='1',url='localhost:8001'):
-        self.model_name = 'arcface_r100_v1'
+    def __init__(self, rec_name = 'arcface_r100_v1', model_version='1', url='10.1.3.166:8001'):
+        self.model_name = rec_name
         self.model_version = model_version
         self.url = url
         self.triton_client = httpclient.InferenceServerClient(url=url, concurrency=1)
 
 
-    def prepare(self,ctx=0):
-        concurrency = 1
+    def prepare(self,ctx_id=0):
+        concurrency = 10
         # Make sure the model matches our requirements, and get some
         # properties of the model that we need for preprocessing
         try:
@@ -104,13 +105,14 @@ class Arcface:
             model_metadata, model_config)
 
 
+
     def get_embedding(self,face_img):
         face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
         face_img = np.transpose(face_img, (2, 0, 1))
         face_img = np.expand_dims(face_img, axis=0)
         face_img = face_img.astype(triton_to_np_dtype(self.dtype))
         inputs = []
-        inputs.append(httpclient.InferInput(self.input_name, [1, self.c, self.h,self.w], "FP32"))
+        inputs.append(httpclient.InferInput(self.input_name, [1, self.c, self.h, self.w], "FP32"))
         inputs[0].set_data_from_numpy(face_img)
 
         out = self.triton_client.infer(self.model_name,
@@ -119,4 +121,51 @@ class Arcface:
                             outputs=None)
         out = [out.as_numpy(e)[0] for e in self.output_name]
         #print(output.get_output(self.output_name)['data'])
+        return np.asarray(out)
+
+
+class DetectorInfer:
+
+    def __init__(self, model = 'retinaface_r50_v1', output_order=None, model_version='1', url='127.0.0.1:8001'):
+        self.model_name = model
+        self.model_version = model_version
+        self.url = url
+        self.input_shape = (1, 3, 640, 640)
+        self.output_order = output_order
+        self.triton_client = httpclient.InferenceServerClient(url=url, concurrency=1)
+
+    # warmup
+    def prepare(self, ctx_id=0):
+        concurrency = 2
+        # Make sure the model matches our requirements, and get some
+        # properties of the model that we need for preprocessing
+        try:
+            model_metadata = self.triton_client.get_model_metadata(
+                model_name=self.model_name, model_version=self.model_version)
+        except InferenceServerException as e:
+            print("failed to retrieve the metadata: " + str(e))
+            sys.exit(1)
+
+        try:
+            model_config = self.triton_client.get_model_config(
+                model_name=self.model_name, model_version=self.model_version)
+        except InferenceServerException as e:
+            print("failed to retrieve the config: " + str(e))
+            sys.exit(1)
+
+        self.max_batch_size, self.input_name, self.output_name, self.c, self.h, self.w, self.format, self.dtype = parse_model_http(
+            model_metadata, model_config)
+
+        self.input_shape = (1, self.c, self.h, self.w)
+
+    def run(self, input):
+        inputs = []
+        inputs.append(httpclient.InferInput(self.input_name, [1, self.c, self.h, self.w], "FP32"))
+        inputs[0].set_data_from_numpy(input)
+        out = self.triton_client.infer(self.model_name,
+                                       inputs,
+                                       model_version=self.model_version,
+                                       outputs=None)
+        out = [out.as_numpy(e) for e in self.output_order]
+
         return out
