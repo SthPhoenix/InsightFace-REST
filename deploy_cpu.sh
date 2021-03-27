b5acc150fb69852739a30b06c8d0f690cb6a807b
SthPhoenix <veloce-raptor@bk.ru>
2021-03-27 19:53:33 +0300

    Update version to 0.5.9.8

--- deploy_cpu.sh
+++ deploy_cpu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 IMAGE='insightface-rest'
-TAG='v0.5.9.6-cpu'
+TAG='v0.5.9.8-cpu'
 
 # Change InsightFace-REST logging level (DEBUG,INFO,WARNING,ERROR)
 log_level=INFO
--- deploy_trt.sh
+++ deploy_trt.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 IMAGE='insightface-rest'
-TAG='v0.5.9.7'
+TAG='v0.5.9.8'
 
 # Change InsightFace-REST logging level (DEBUG,INFO,WARNING,ERROR)
 log_level=INFO
