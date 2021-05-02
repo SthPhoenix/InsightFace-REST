1f90b65862a1cfbef6888d9f2cf51b93dc9c7a04
SthPhoenix <veloce-raptor@bk.ru>
2021-05-03 00:10:03 +0300

    Replace outputs parameter with kwargs

--- src/api_trt/modules/model_zoo/face_processors.py
+++ src/api_trt/modules/model_zoo/face_processors.py
@@ -2,31 +2,31 @@ from .detectors.retinaface import RetinaFace
 from .detectors.centerface import CenterFace
 
 
-def arcface_r100_v1(model_path, backend, outputs):
+def arcface_r100_v1(model_path, backend, **kwargs):
     model = backend.Arcface(rec_name=model_path)
     return model
 
 
-def r50_arcface_msfdrop75(model_path, backend, outputs):
+def r50_arcface_msfdrop75(model_path, backend, **kwargs):
     model = backend.Arcface(rec_name=model_path)
     return model
 
 
-def r100_arcface_msfdrop75(model_path, backend, outputs):
+def r100_arcface_msfdrop75(model_path, backend, **kwargs):
     model = backend.Arcface(rec_name=model_path)
     return model
 
 
-def glint360k_r100FC_1_0(model_path, backend, outputs):
+def glint360k_r100FC_1_0(model_path, backend, **kwargs):
     model = backend.Arcface(rec_name=model_path)
     return model
 
 
-def glint360k_r100FC_0_1(model_path, backend, outputs):
+def glint360k_r100FC_0_1(model_path, backend, **kwargs):
     model = backend.Arcface(rec_name=model_path)
     return model
 
 
-def genderage_v1(model_path, backend, outputs):
+def genderage_v1(model_path, backend, **kwargs):
     model = backend.FaceGenderage(rec_name=model_path)
     return model
