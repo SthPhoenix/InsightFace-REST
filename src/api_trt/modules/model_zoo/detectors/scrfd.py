699f81ce3ee4e179fc3aa9fb4a2b28cea061e4bf
SthPhoenix <veloce-raptor@bk.ru>
2021-11-04 23:34:17 +0300

    Faster SCRFD re-implementation using Numba

--- src/api_trt/modules/model_zoo/detectors/scrfd.py
+++ src/api_trt/modules/model_zoo/detectors/scrfd.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
-# @Organization  : insightface.ai
-# @Author        : Jia Guo
-# @Time          : 2021-05-04
-# @Function      : 
+# Based on Jia Guo reference implementation at
+# https://github.com/deepinsight/insightface/blob/master/detection/scrfd/tools/scrfd.py
 
-# Modified for InsightFace-REST
 
 from __future__ import division
+import time
+from typing import Union
+from functools import wraps
+import logging
+
+import cv2
 import numpy as np
 from numba import njit
-
-import time
-import os.path as osp
-import cv2
-from typing import Union
-import logging
 
 from .common.nms import nms
 from ..exec_backends.onnxrt_backend import DetectorInfer as DIO
@@ -23,11 +20,23 @@ from ..exec_backends.onnxrt_backend import DetectorInfer as DIO
 try:
     import cupy as cp
     from ..exec_backends.trt_backend import DetectorInfer as DIT
-except:
+except BaseException:
     DIT = None
 
 
-@njit(cache=True, fastmath=True)
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        t0 = time.time()
+        result = f(*args, **kw)
+        took_ms = (time.time() - t0) * 1000
+        logging.debug(f'func: "{f.__name__}" took: {took_ms:.4f} ms')
+        return result
+
+    return wrap
+
+
+@njit(fastmath=True, cache=True)
 def distance2bbox(points, distance):
     """Decode distance prediction to bounding box.
 
@@ -52,7 +61,7 @@ def distance2bbox(points, distance):
     return distance
 
 
-@njit(cache=True, fastmath=True)
+@njit(fastmath=True, cache=True)
 def distance2kps(points, distance):
     """Decode distance prediction to bounding box.
 
@@ -76,7 +85,124 @@ def distance2kps(points, distance):
     return distance
 
 
+@njit(fastmath=True, cache=True)
+def fast_multiply(matrix, value):
+    """
+    Fast multiply list on value
+
+    :param matrix: List of values
+    :param value: Multiplier
+    :return: Multiplied list
+    """
+    for ix in range(0, matrix.shape[0]):
+        matrix[ix] *= value
+    return matrix
+
+
+@njit(fastmath=True, cache=True)
+def single_distance2bbox(point, distance):
+    """
+    Fast conversion of single bbox distances to coordinates
+
+    :param point: Anchor point
+    :param distance: Bbox distances from anchor point
+    :return: bbox
+    """
+    distance[0] = point[0] - distance[0]
+    distance[1] = point[1] - distance[1]
+    distance[2] = point[0] + distance[2]
+    distance[3] = point[1] + distance[3]
+    return distance
+
+
+@njit(fastmath=True, cache=True)
+def single_distance2kps(point, distance):
+    """
+    Fast conversion of single keypoint distances to coordinates
+
+    :param point: Anchor point
+    :param distance: Keypoint distances from anchor point
+    :return: keypoint
+    """
+    for ix in range(0, distance.shape[0], 2):
+        distance[ix] += point[0]
+        distance[ix + 1] += point[1]
+    return distance
+
+
+@njit(fastmath=True, cache=True)
+def generate_proposals(score_blob, bbox_blob, kpss_blob, stride, anchors, threshold, score_out, bbox_out, kpss_out,
+                       offset):
+    """
+    Convert distances from anchors to actual coordinates on source image
+    and filter proposals by confidence threshold.
+    Uses preallocated np.ndarrays for output.
+
+    :param score_blob: Raw scores for stride
+    :param bbox_blob: Raw bbox distances for stride
+    :param kpss_blob: Raw keypoints distances for stride
+    :param stride: Stride scale
+    :param anchors: Precomputed anchors for stride
+    :param threshold: Confidence threshold
+    :param score_out: Output scores np.ndarray
+    :param bbox_out: Output bbox np.ndarray
+    :param kpss_out: Output key points np.ndarray
+    :param offset: Write offset for output arrays
+    :return:
+    """
+
+    total = offset
+
+    for ix in range(0, anchors.shape[0]):
+        if score_blob[ix, 0] > threshold:
+            bbox = fast_multiply(bbox_blob[ix], stride)
+            kpss = fast_multiply(kpss_blob[ix], stride)
+            bbox = single_distance2bbox(anchors[ix], bbox)
+            kpss = single_distance2kps(anchors[ix], kpss)
+            score_out[total] = score_blob[ix]
+            bbox_out[total] = bbox
+            kpss_out[total] = kpss
+            total += 1
+
+    return score_out, bbox_out, kpss_out, total
+
+
+# @timing
+@njit(fastmath=True, cache=True)
+def filter(bboxes_list: np.ndarray, kpss_list: np.ndarray,
+           scores_list: np.ndarray):
+    """
+    Filter postprocessed network outputs with NMS
+
+    :param bboxes_list: List of bboxes (np.ndarray)
+    :param kpss_list: List of keypoints (np.ndarray)
+    :param scores_list: List of scores (np.ndarray)
+    :return: Face bboxes with scores [t,l,b,r,score], and key points
+    """
+
+    scores_ravel: np.ndarray = scores_list.reshape(-1)
+    order: np.ndarray = scores_ravel.argsort()[::-1]
+    pre_det = np.hstack((bboxes_list, scores_list))
+    pre_det = pre_det[order, :]
+    keep = nms(pre_det)
+    keep = np.asarray(keep)
+    det = pre_det[keep, :]
+    kpss = kpss_list.reshape((kpss_list.shape[0], -1, 2))
+    kpss = kpss[order, :, :]
+    kpss = kpss[keep, :, :]
+
+    return det, kpss
+
+
 def _normalize_on_device(input, stream, out):
+    """
+    Normalize image on GPU using inference backend preallocated buffers
+
+    :param input: Raw image as nd.ndarray with HWC shape
+    :param stream: Inference backend CUDA stream
+    :param out: Inference backend pre-allocated input buffer
+    :return: Image shape after preprocessing
+    """
     input = np.expand_dims(input, axis=0)
     allocate_place = np.prod(input.shape)
     with stream:
@@ -89,15 +215,14 @@ def _normalize_on_device(input, stream, out):
 
 
 class SCRFD:
+
     def __init__(self, inference_backend: Union[DIT, DIO], ver=1):
         self.session = inference_backend
         self.center_cache = {}
         self.nms_threshold = 0.4
         self.masks = False
         self.ver = ver
-        self.use_kps = True
         self.out_shapes = None
-        self.batched = False
         self._anchor_ratio = 1.0
         self.fmc = 3
         self._feat_stride_fpn = [8, 16, 32]
@@ -106,133 +231,177 @@ class SCRFD:
         self.input_ptr = None
 
     def prepare(self, nms_treshold: float = 0.45, **kwargs):
+        """
+        Read network params and populate class parameters
+
+        :param nms_treshold: Threshold for NMS IoU
+
+        """
         self.nms_threshold = nms_treshold
         self.session.prepare()
         self.out_shapes = self.session.out_shapes
-        if len(self.out_shapes[0]) == 3:
-            self.batched = True
         self.input_shape = self.session.input_shape
         self.infer_shape = self.input_shape
+
+        # Preallocate reusable arrays for proposals
+        max_prop_len = self._get_max_prop_len(self.input_shape,
+                                              self._feat_stride_fpn,
+                                              self._num_anchors)
+        self.score_list = np.zeros((max_prop_len, 1))
+        self.bbox_list = np.zeros((max_prop_len, 4))
+        self.kpss_list = np.zeros((max_prop_len, 10))
+
         # Check if exec backend provides CUDA stream
         try:
             self.stream = self.session.stream
             self.input_ptr = self.session.input_ptr
-        except:
+        except BaseException:
             pass
 
-    def preprocess(self, img):
+    # @timing
+    def detect(self, img, threshold=0.5):
+        """
+        Run detection pipeline for provided image
+
+        :param img: Raw image as nd.ndarray with HWC shape
+        :param threshold: Confidence threshold
+        :return: Face bboxes with scores [t,l,b,r,score], and key points
+        """
+
+        input_height = img.shape[0]
+        input_width = img.shape[1]
+        blob = self._preprocess(img)
+        net_outs = self._forward(blob)
+
+        bboxes_list, kpss_list, scores_list = self._postprocess(net_outs, input_height, input_width, threshold)
+
+        det, kpss = filter(
+            bboxes_list, kpss_list, scores_list)
+
+        return det, kpss
+
+    @staticmethod
+    def _get_max_prop_len(input_shape, feat_strides, num_anchors):
+        """
+        Estimate maximum possible number of proposals returned by network
+
+        :param input_shape: maximum input shape of model (i.e (1, 3, 640, 640))
+        :param feat_strides: model feature strides (i.e. [8, 16, 32])
+        :param num_anchors: model number of anchors (i.e 2)
+        :return:
+        """
+
+        ln = 0
+        pixels = input_shape[2] * input_shape[3]
+        for e in feat_strides:
+            ln += pixels / (e * e) * num_anchors
+        return int(ln)
+
+    # @timing
+    @staticmethod
+    def _build_anchors(input_height, input_width, strides, num_anchors):
+        """
+        Precompute anchor points for provided image size
+
+        :param input_height: Input image height
+        :param input_width: Input image width
+        :param strides: Model strides
+        :param num_anchors: Model num anchors
+        :return: box centers
+        """
+
+        centers = []
+        for stride in strides:
+            height = input_height // stride
+            width = input_width // stride
+
+            anchor_centers = np.stack(np.mgrid[:height, :width][::-1], axis=-1).astype(np.float32)
+            anchor_centers = (anchor_centers * stride).reshape((-1, 2))
+            if num_anchors > 1:
+                anchor_centers = np.stack([anchor_centers] * num_anchors, axis=1).reshape((-1, 2))
+            centers.append(anchor_centers)
+        return centers
+
+    # @timing
+    def _preprocess(self, img):
+        """
+        Normalize image on CPU if backend can't provide CUDA stream,
+        otherwise preprocess image on GPU using CuPy
+
+        :param img: Raw image as np.ndarray with HWC shape
+        :return: Preprocessed image or None if image was processed on device
+        """
+
         blob = None
-        if self.ver == 2:
-            blob = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            blob = np.transpose(blob, (2, 0, 1))
-            blob = np.expand_dims(blob, axis=0).astype(self.session.input_dtype)
+        if self.stream:
+            self.infer_shape = _normalize_on_device(
+                img, self.stream, self.input_ptr)
         else:
-            if self.stream:
-                self.infer_shape = _normalize_on_device(img, self.stream, self.input_ptr)
-            else:
-                input_size = tuple(img.shape[0:2][::-1])
-                blob = cv2.dnn.blobFromImage(img, 1.0 / 128, input_size, (127.5, 127.5, 127.5), swapRB=True)
+            input_size = tuple(img.shape[0:2][::-1])
+            blob = cv2.dnn.blobFromImage(
+                img, 1.0 / 128, input_size, (127.5, 127.5, 127.5), swapRB=True)
         return blob
 
-    def forward(self, blob):
+    def _forward(self, blob):
+        """
+        Send input data to inference backend.
+
+        :param blob: Preprocessed image of shape NCHW or None
+        :return: network outputs
+        """
+
         t0 = time.time()
         if self.stream:
-            net_outs = self.session.run(from_device=True, infer_shape=self.input_shape)
+            net_outs = self.session.run(
+                from_device=True, infer_shape=self.input_shape)
         else:
             net_outs = self.session.run(blob)
         t1 = time.time()
         logging.debug(f'Inference cost: {(t1 - t0) * 1000:.3f} ms.')
         return net_outs
 
-    def postprocess(self, input_height, input_width, net_outs, threshold):
-        scores_list = []
-        bboxes_list = []
-        kpss_list = []
-        fmc = self.fmc
+    # @timing
+    def _postprocess(self, net_outs, input_height, input_width, threshold):
+        """
+        Precompute anchor points for provided image size and process network outputs
+
+        :param net_outs: Network outputs
+        :param input_height: Input image height
+        :param input_width: Input image width
+        :param threshold: Confidence threshold
+        :return: filtered bboxes, keypoints and scores
+        """
+
+        key = (input_height, input_width)
+
+        if not self.center_cache.get(key):
+            self.center_cache[key] = self._build_anchors(input_height, input_width, self._feat_stride_fpn,
+                                                         self._num_anchors)
+        anchor_centers = self.center_cache[key]
+        bboxes, kpss, scores = self._process_strides(net_outs, threshold, anchor_centers)
+        return bboxes, kpss, scores
+
+    def _process_strides(self, net_outs, threshold, anchor_centers):
+        """
+        Process network outputs by strides and return results proposals filtered by threshold
+
+        :param net_outs: Network outputs
+        :param threshold: Confidence threshold
+        :param anchor_centers: Precomputed anchor centers for all strides
+        :return: filtered bboxes, keypoints and scores
+        """
+
+        offset = 0
+
         for idx, stride in enumerate(self._feat_stride_fpn):
-            kps_preds = None
-            if self.batched:
-                scores = net_outs[idx][0]
-                bbox_preds = net_outs[idx + fmc][0] * stride
-                if self.use_kps:
-                    kps_preds = net_outs[idx + fmc * 2][0] * stride
-            else:
-                scores = net_outs[idx]
-                bbox_preds = net_outs[idx + fmc]
-                bbox_preds = bbox_preds * stride
-                if self.use_kps:
-                    kps_preds = net_outs[idx + fmc * 2] * stride
-
-            height = input_height // stride
-            width = input_width // stride
-            key = (height, width, stride)
-            if key in self.center_cache:
-                anchor_centers = self.center_cache[key]
-            else:
-                anchor_centers = np.stack(np.mgrid[:height, :width][::-1], axis=-1).astype(np.float32)
-                anchor_centers = (anchor_centers * stride).reshape((-1, 2))
-                if self._num_anchors > 1:
-                    anchor_centers = np.stack([anchor_centers] * self._num_anchors, axis=1).reshape((-1, 2))
-                if len(self.center_cache) < 100:
-                    self.center_cache[key] = anchor_centers
-
-            pos_inds = np.where(scores >= threshold)[0]
-            bboxes = distance2bbox(anchor_centers, bbox_preds)
-            pos_scores = scores[pos_inds]
-            pos_bboxes = bboxes[pos_inds]
-            scores_list.append(pos_scores)
-            bboxes_list.append(pos_bboxes)
-            if self.use_kps:
-                kpss = distance2kps(anchor_centers, kps_preds)
-                kpss = kpss.reshape((kpss.shape[0], -1, 2))
-                pos_kpss = kpss[pos_inds]
-                kpss_list.append(pos_kpss)
-        return bboxes_list, kpss_list, scores_list
-
-    def filter(self, bboxes_list, kpss_list, scores_list, img_center, max_num, metric):
-        scores = np.vstack(scores_list)
-        scores_ravel = scores.ravel()
-        order = scores_ravel.argsort()[::-1]
-        bboxes = np.vstack(bboxes_list)
-        pre_det = np.hstack((bboxes, scores)).astype(np.float32, copy=False)
-        pre_det = pre_det[order, :]
-        keep = nms(pre_det)
-        det = pre_det[keep, :]
-        if self.use_kps:
-            kpss = np.vstack(kpss_list)
-            kpss = kpss[order, :, :]
-            kpss = kpss[keep, :, :]
-        else:
-            kpss = None
-        if max_num > 0 and det.shape[0] > max_num:
-            area = (det[:, 2] - det[:, 0]) * (det[:, 3] -
-                                              det[:, 1])
-
-            if metric == 'max':
-                values = area
-            else:
-
-                offsets = np.vstack([
-                    (det[:, 0] + det[:, 2]) / 2 - img_center[1],
-                    (det[:, 1] + det[:, 3]) / 2 - img_center[0]
-                ])
-                offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
-                values = area - offset_dist_squared * 2.0  # some extra weight on the centering
-
-            bindex = np.argsort(values)[::-1]  # some extra weight on the centering
-            bindex = bindex[0:max_num]
-            det = det[bindex, :]
-            if kpss is not None:
-                kpss = kpss[bindex, :]
-        return det, kpss
-
-    def detect(self, img, threshold=0.5, max_num=0, metric='default'):
-        input_height = img.shape[0]
-        input_width = img.shape[1]
-        img_center = img.shape[0] // 2, img.shape[1] // 2
-        blob = self.preprocess(img)
-        net_outs = self.forward(blob)
-        bboxes_list, kpss_list, scores_list = self.postprocess(input_height, input_width, net_outs, threshold)
-        det, kpss = self.filter(bboxes_list, kpss_list, scores_list, img_center, max_num, metric)
-
-        return det, kpss
+            score_blob = net_outs[idx][0]
+            bbox_blob = net_outs[idx + self.fmc][0]
+            kpss_blob = net_outs[idx + self.fmc * 2][0]
+            stride_anchors = anchor_centers[idx]
+            self.score_list, self.bbox_list, self.kpss_list, total = generate_proposals(score_blob, bbox_blob,
+                                                                                        kpss_blob, stride,
+                                                                                        stride_anchors, threshold,
+                                                                                        self.score_list, self.bbox_list,
+                                                                                        self.kpss_list, offset)
+            offset = total
+        return self.bbox_list[:offset], self.kpss_list[:offset], self.score_list[:offset]
