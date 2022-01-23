d335238707ed66c08ed177d32ccc8e5e8924700e
SthPhoenix <sthphx2048@gmail.com>
2022-01-23 16:30:54 +0300

    Increase dafault timeout-keep-alive to 20s. Shuild fix #69

--- src/api_trt/entrypoint.sh
+++ src/api_trt/entrypoint.sh
@@ -4,4 +4,4 @@ echo Preparing models...
 python prepare_models.py
 
 echo Starting InsightFace-REST using $NUM_WORKERS workers.
-uvicorn app:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS
+uvicorn app:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS  --timeout-keep-alive 20000
