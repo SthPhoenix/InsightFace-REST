9b96e011c4a372e5506d22e766f659c74d9cb9d5
SthPhoenix <sthphx2048@gmail.com>
2022-05-22 19:54:46 +0300

    Replace uvicorn server with gunicorn. Improves throughput in most cases

--- src/api_trt/entrypoint.sh
+++ src/api_trt/entrypoint.sh
@@ -4,4 +4,5 @@ echo Preparing models...
 python prepare_models.py
 
 echo Starting InsightFace-REST using $NUM_WORKERS workers.
-uvicorn app:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS  --timeout-keep-alive 20000
+#uvicorn app:app --port 18080 --host 0.0.0.0 --workers $NUM_WORKERS  --timeout-keep-alive 20000
+gunicorn --log-level $LOG_LEVEL -w $NUM_WORKERS -k uvicorn.workers.UvicornWorker --keep-alive 20 app:app -b 0.0.0.0:18080
