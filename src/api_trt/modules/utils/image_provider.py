6e8224f0e04959dd7c49f730aba9b1d8a308ecee
SthPhoenix <sthphx2048@gmail.com>
2022-06-13 20:43:24 +0300

    Add follow_redirects=True for httpx client

--- src/api_trt/modules/utils/image_provider.py
+++ src/api_trt/modules/utils/image_provider.py
@@ -27,7 +27,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 }
 
-client = httpx.AsyncClient(headers=headers)
+client = httpx.AsyncClient(headers=headers, follow_redirects=True)
 
 def read_as_bytes(path, **kwargs):
     with open(path, mode='rb') as fl:
