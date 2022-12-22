50334fc627d6b1f1b54b7d56dafbda06f08d0772
SthPhoenix <sthphx2048@gmail.com>
2022-12-22 23:41:57 +0300

    update version

--- src/api_trt/app.py
+++ src/api_trt/app.py
@@ -18,7 +18,7 @@ from modules.processing import Processing
 from schemas import BodyDraw, BodyExtract
 from settings import Settings
 
-__version__ = "0.8.0.1"
+__version__ = "0.8.1.0"
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
